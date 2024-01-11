62663862c2f397d2fb9c1f5bc8cc0a43e794c1ac
Lucas DUPAS <dupaslucas8@gmail.com>
2024-01-11 14:19:18 +0100

    Ajout effet chevron matieres

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,8 @@
 import './styles/app.scss'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+
+
+
+
+
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
