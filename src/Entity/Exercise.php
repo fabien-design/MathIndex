86228f3f2781da7784282a049ed8590b0d3712ad
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2024-04-03 14:19:12 +0200

    fix assetNotBlank + td to tr to fix weird borders

--- src/Entity/Exercise.php
+++ src/Entity/Exercise.php
@@ -6,6 +6,7 @@ use App\Repository\ExerciseRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ExerciseRepository::class)]
 #[Vich\Uploadable]
