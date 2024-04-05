7870b5eb05cec67fb7fd30eefcb538d219f49c8e
TWhiteShadow <thewhiteshadowgaming@gmail.com>
2024-04-05 18:51:55 +0200

    adding nouveauté in exercises

--- assets/js/keywordsSplit.js
+++ assets/js/keywordsSplit.js
@@ -3,6 +3,7 @@ export function keywordsSplit(){
     document.querySelectorAll(".keywordsContainer").forEach((element) => {
         let keywords = element.innerText;
         let splittedKeywords = keywords.split("@");
+        console.log(splittedKeywords);
         element.innerHTML = "";
         splittedKeywords.forEach((keyword) => {
             element.innerHTML += `<span class="keyword">${keyword}</span>`;
--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,24 +3,50 @@
 namespace App\Controller;
 
 use App\Entity\Course;
+use DateTime;
+use DateTimeImmutable;
+use DateTimeZone;
+use Knp\Component\Pager\PaginatorInterface;
 use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CourseController extends AbstractController
 {
     #[Route('/matiere/{course}', name: 'app_course')]
-    public function index(Course $course, CourseRepository $courseRepository): Response
+    public function index(Course $course, Request $request, PaginatorInterface $paginator, CourseRepository $courseRepository): Response
     {
-
-        $exercises = $course->getExercises();
+        
         $courses = $courseRepository->findAll();
+        $exercises = $course->getExercises();
+        
+        $pagination = $paginator->paginate(
+            $exercises,
+            $request->query->getInt('page', 1),
+            2
+        );
+        $newExercises = [];
+        if($request->query->getInt('page') == 0){
+            $original = new DateTime("now", new DateTimeZone('UTC'));
+            $timezoneName = timezone_name_from_abbr("", 1*3600, false);
+            $modifiedDate = $original->setTimezone(new DateTimezone($timezoneName));
+            $modifiedDate->modify("-1 day");
+            
+            foreach ($exercises as $exercise) {
+                $exerciseDate = new \DateTime($exercise->getCreatedAt());
+                if ($exerciseDate > $modifiedDate) {
+                    array_push($newExercises, $exercise);
+                }
+            }
+        }
         return $this->render('course/index.html.twig', [
             'controller_name' => 'CourseController',
             'course' => $course,
             'courses' => $courses,
-            'exercises' => $exercises,
+            'exercises' => $pagination,
+            'newExercises' => $newExercises,
         ]);
     }
 }
